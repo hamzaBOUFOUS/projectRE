a77727019c4c5b28dd03e36a8d7a56bf9240394b
hamza boufous <38819898+hamzaBOUFOUS@users.noreply.github.com>
2021-06-07 23:39:59 +0100

    version 5

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,18 @@
+import React, { lazy } from "react";
+import { Route, Switch } from "react-router-dom";
+import EvenementPage from "../pages/EvenementPage";
+import ContratPage from "../pages/ContratPage";
+import DepartmentPage from "../pages/DepartmentPage";
+import DocumentPage from "../pages/DocumentPage";
+import PostePage from "../pages/PostePage";
+
+const Routes = () => (
+    <Switch>
+        <Route path='/evenement' component={EvenementPage} />
+        <Route path='/contrat' component={ContratPage} />
+        <Route path='/department' component={DepartmentPage} />
+        <Route path='/document' component={DocumentPage} />
+        <Route path='/poste' component={PostePage} />
+    </Switch>
+);
+export default Routes;
